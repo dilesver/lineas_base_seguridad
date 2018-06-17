65138ebb04724913eb83f32625b41c39d91e6c61
Dilson Cesar Escobar Vergara <dilesver@gmail.com>
2018-06-17 18:50:02 -0500

    Dehabilitar conexión a BD MySQL

--- lineas_base_seguridad/settings.py
+++ lineas_base_seguridad/settings.py
@@ -87,15 +87,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-	
-	#'default': {
-	#	'ENGINE': 'django.db.backends.mysql',
-    #    'HOST': 'localhost',
-    #    'NAME': 'lineas_base_seguridad',
-    #    'USER': 'root',
-    #    'PASSWORD': '',
-    #    'PORT': 3306,
-    #}
 }
 
 
